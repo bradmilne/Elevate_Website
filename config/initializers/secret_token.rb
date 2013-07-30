46967aedaf624159eb2cf15fa74d13ca9f7ad9d5
Brad Milne <brad.milne@getelevate.com>
2013-07-30 13:11:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elevate::Application.config.secret_token = '014122ba337bea6d51e97327b10709f234c891737b6d9b8058e662c9a2a688ceeab185e3ec26530d37c9b977e8e1ff685d53ed6f432ac76e297bf85023feb033'
