674ad16de2998fdc17328d1f2d403ca09dd2fe4f
Brad Milne <brad.milne@getelevate.com>
2014-08-05 21:27:08 -0400

    add eloqua analytics

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
   default from: "support@getelevate.com"
-  default to: ["brad.milne@getelevate.com", "sandy.gibson@getelevate.com"]
+  default to: ["brad.milne@getelevate.com", "sandy.gibson@getelevate.com", "joe.okane@getelevate.com"]
 
   def notify_sales(name, company, email, phone)
   	@name = name
